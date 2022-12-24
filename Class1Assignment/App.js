5131562bae389333f81703b781597464b2a168c6
Pratiksha Mhatre <mpratiksha@deaninfotech.com>
2022-12-24 22:30:54 +0530

    Initial Commit

---
+++ Class1Assignment/App.js
@@ -0,0 +1,13 @@
+const h1 = React.createElement(
+  "h1",
+  { style: { color: "red" } },
+  "This is heading 1"
+);
+const h2 = React.createElement(
+  "h2",
+  { className: "heading2" },
+  "This is heading 2"
+);
+const container = React.createElement("div", {}, [h1, h2]);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(container);
